ccbbee0a3a07c78df33988b55d0237adc17854e9
mvksim95 <maxime.tyson@gmail.com>
2024-12-23 20:01:58 +0100

    ajout book ok, delete créé, à tester ensuite

--- controllers/book.js
+++ controllers/book.js
@@ -1,13 +1,21 @@
 const Book = require ('../models/Book');
+const fs = require ('fs');
 
 exports.createBook = (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body
-    });
-    book.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
+  console.log('DEBUG req.body.book : ', req.body);
+  console.log('DEBUG 2 image : ', req.file);
+  const bookObject = JSON.parse(req.body.book);
+  delete bookObject._id;
+  delete bookObject._userId;
+  const book = new Book({
+      ...bookObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+
+  book.save()
+  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+  .catch(error => { res.status(400).json( { error })})
 };
 
 exports.getAllBooks = (req, res, next) => {
@@ -23,3 +31,27 @@ exports.getAllBooks = (req, res, next) => {
         }
       );
 }; 
+
+
+// a tester 
+exports.deleteBook = (req, res, next) => {
+
+  Book.findOne({ _id: req.params.id })
+    .then(book => {
+      if (book.userID != req.auth.userID) {
+        res.status(401).json({ message: 'Non-autorisé' });
+      } else {
+        const filename = book.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Book.deleteOne({ _id: req.params.id })
+            .then(() => {
+              res.status(200).json({ message: 'Objet supprimé !' });
+            })
+            .catch(error => res.status(401).json({ error }));
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+};
--- routes/book.js
+++ routes/book.js
@@ -1,11 +1,11 @@
 const express = require ('express');
 const router = express.Router();
-
 const auth = require('../middleware/auth');
+const multer = require ('../middleware/multer-config');
 
 const bookCtrl = require ('../controllers/book');
 
-router.post('/', auth, bookCtrl.createBook);
+router.post('/', auth, multer, bookCtrl.createBook)
 
 router.get('/', bookCtrl.getAllBooks);
 
